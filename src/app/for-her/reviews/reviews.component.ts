3bb7e54d0929dcb2348a4f47cf626075e1753533
SerebryanskiySergei <serebryanskiysergei@gmail.com>
2020-07-07 19:52:30 +0300

    Added office description

--- src/app/for-her/reviews/reviews.component.ts
+++ src/app/for-her/reviews/reviews.component.ts
@@ -11,29 +11,28 @@ export class ReviewsComponent implements OnInit {
 
   customOptions: OwlOptions = {
     loop:true,
-		margin:30,
-		nav: true,
+	margin:30,
+	nav: true,
     dots: true,
     autoplay:true,
     autoplayTimeout:1000,
-		autoplaySpeed: 2000,
+	autoplaySpeed: 2000,
     navSpeed: 2000,
-		navText: ['<i class="flaticon-left-arrow"></i>', '<i class="flaticon-right-arrow"></i>'],
-		responsive:{
-			0:{
-				items:1
-			},
-			
-			480:{
-				items:2
-			},			
-			
-			991:{
-				items:2
-			},
-			1000:{
-				items:3
-			}
+	navText: ['<i class="flaticon-left-arrow"></i>', '<i class="flaticon-right-arrow"></i>'],
+	responsive:{
+		0:{
+			items:1
+		},
+		480:{
+			items:2
+		},			
+		
+		991:{
+			items:2
+		},
+		1000:{
+			items:3
+		}
     }
   }
 
